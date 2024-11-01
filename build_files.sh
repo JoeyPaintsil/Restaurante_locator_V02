e0584c336447b9aff626527b4ca4ea2766cbeb23
JoeyPaintsil <paintsil610@gmail.com>
2024-11-01 18:37:14 +0000

    Added changes 3

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-pip3 install -r requirements.txt
-python3 manage.py collectstatic --noinput
-python3 manage.py migrate
+# Build the project
+echo "Building the project..."
+python3.9 -m pip install -r requirements.txt
+
+echo "Make Migration..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+echo "Collect Static..."
+python3.9 manage.py collectstatic --noinput --clear
